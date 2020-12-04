cc0defb2bdf2652a298b0aad762de2f3ca502cbc
ambetack <64190268+ambetack@users.noreply.github.com>
2020-12-04 14:23:40 +0530

    Add files via upload

---
+++ instapost/postdetail.js
@@ -0,0 +1,28 @@
+const http = require("https");
+
+const options = {
+	"method": "GET",
+	"hostname": "instagram47.p.rapidapi.com",
+	"port": null,
+	"path": "/post_details/?shortcode=CHLXcX-h-Px",
+	"headers": {
+		"x-rapidapi-key": "bc0820044bmshfa665d7d7b682fep1db460jsnccdf1ee613e4",
+		"x-rapidapi-host": "instagram47.p.rapidapi.com",
+		"useQueryString": true
+	}
+};
+
+const req = http.request(options, function (res) {
+	const chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		const body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
